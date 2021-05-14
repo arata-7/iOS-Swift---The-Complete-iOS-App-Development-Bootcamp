cc4b83702800f93cd0e32a06b82d763457501d72
art-77 <arata.tanaka8@gmail.com>
2021-05-14 21:24:36 +0900

    cources already finished 2020

---
+++ BMI-Calculator-iOS13/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by 田中新 on 2021/01/13.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
